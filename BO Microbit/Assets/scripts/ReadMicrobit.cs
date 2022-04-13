ebc6f7dd71edfe71b70e26ec7e8293fa96081fd9
RossTubes <70896941+RossTubes@users.noreply.github.com>
2022-04-13 15:21:08 +0200

    Update ReadMicrobit.cs
    adding buttons!

--- BO Microbit/Assets/scripts/ReadMicrobit.cs
+++ BO Microbit/Assets/scripts/ReadMicrobit.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO.Ports;
+using System.Threading.Tasks;
 public class ReadMicrobit : MonoBehaviour
 {
     private SerialPort serial; // Start is called before the first frame update
@@ -10,8 +11,27 @@ public class ReadMicrobit : MonoBehaviour
         get { return y; }
 
     }
+    public bool ButtonPressed
+    {
+        get { return button; }
 
+    }
+    public bool ButtonPressedB
+    {
+        get { return buttonB; }
+
+    }
+    public int xAxis
+    {
+        get { return x; }
+
+    }
+    private int x;
+    private bool button;
+    private bool buttonB;
     private int y;
+
+    public bool continueReading = true;
     void Awake()
     {
         string[] portNames = SerialPort.GetPortNames();
@@ -26,19 +46,35 @@ public class ReadMicrobit : MonoBehaviour
         serial.StopBits = StopBits.One;
         serial.Parity = Parity.None;
         serial.Handshake = Handshake.None; //confirmation for sending
-
+        //serial.ReadTimeout = 100;
         serial.Open();
+
+       // ReadSerial();
     }
+
     private void Update()
     {
-        string data = serial.ReadLine();
-        Debug.Log(data); //string waardes halen converten naar int
-        int x;
+        ParseSerial();
+    }
 
-        if (data.Contains("X-axis:"))
+    public async void ReadSerial()
+    {
+        while (continueReading)
+        {
+        ParseSerial();
+        await Task.Delay(20);
+
+        }
+    }
+    public async void ParseSerial()
+    {
+        string data = serial.ReadLine();
+        //Debug.Log(data); //string waardes halen converten naar int
+
+        if (data.Contains("X:"))
         {
             string substr;
-            int index = data.IndexOf("X-axis:") + 7;
+            int index = data.IndexOf("X:") + 2;
             int end = data.IndexOf('|', index);
 
             if (end == -1)
@@ -50,14 +86,14 @@ public class ReadMicrobit : MonoBehaviour
                 substr = data.Substring(index, end - index);
             }
             x = Int32.Parse(substr);
-            Debug.Log($"X: {substr}");
+            //Debug.Log($"X: {substr}");
         }
         //int y;
 
-        if (data.Contains("Y-axis:"))
+        if (data.Contains("Y:"))
         {
             string substr;
-            int index = data.IndexOf("Y-axis:") + 7; //gets index out of array
+            int index = data.IndexOf("Y:") + 2; //gets index out of array
             int end = data.IndexOf('|', index); //get first index of character after specified index
 
             if (end == -1)
@@ -71,8 +107,43 @@ public class ReadMicrobit : MonoBehaviour
 
             y = Int32.Parse(substr); //transformed string to int
 
-            Debug.Log($"Y: {substr}");
+            //Debug.Log($"Y: {substr}");
+        }
+        if (data.Contains("G:"))
+        {
+            string substr;
+            int index = data.IndexOf("G:") + 2;
+            int end = data.IndexOf('|', index);
+
+            if (end == -1)
+            {
+                substr = data.Substring(index);
+            }
+            else
+            {
+                substr = data.Substring(index, end - index);
+            }
+            button = bool.Parse(substr);
+            //  Debug.Log($"Button: {substr}");
+        }
+        if (data.Contains("B:"))
+        {
+            string substr;
+            int index = data.IndexOf("B:") + 2;
+            int end = data.IndexOf('|', index);
+
+            if (end == -1)
+            {
+                substr = data.Substring(index);
+            }
+            else
+            {
+                substr = data.Substring(index, end - index);
+            }
+            buttonB = bool.Parse(substr);
+            // Debug.Log($"ButtonB: {substr}");
         }
     }
+    
 }
 
